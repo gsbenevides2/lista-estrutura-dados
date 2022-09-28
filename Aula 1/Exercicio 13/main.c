0d6ca880214f80d5c92d4c49314dc7542c6c5e72
Guilherme da Silva Benevides <45762112+gsbenevides2@users.noreply.github.com>
2022-09-28 20:20:54 -0300

    Aula 1 Exercício 13

---
+++ Aula 1/Exercicio 13/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void troca(int *a, int *b){
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+
+int main(void){
+    int x = 5;
+    int y = 3;
+
+    printf("x = %d , y = %d\n",x,y);
+    troca(&x,&y);
+    printf("x = %d , y = %d\n",x,y);
+    return 0;
+}
